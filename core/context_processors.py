0cf168b1f7bac6afbc9d3904742aaef52bd834b3
Mihail-Popescu <mihaixz4@gmail.com>
2026-02-18 18:35:16 +0200

    updated version

--- core/context_processors.py
+++ core/context_processors.py
@@ -12,4 +12,4 @@ def nav_items(request):
 
 
 def version_context(request):
-    return {"version": "v1.16.0"}  # Update this with each release
+    return {"version": "v1.17.0"}  # Update this with each release
